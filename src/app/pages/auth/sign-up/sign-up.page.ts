5fafa57ee09b55f1a0b2afb10b144b78338915e5
CristinaOsorio <macry_08@hotmail.com>
2024-06-21 22:38:46 -0600

    feat: add user registration functionality

--- src/app/pages/auth/sign-up/sign-up.page.ts
+++ src/app/pages/auth/sign-up/sign-up.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FirebaseService } from './../../../services/firebase.service';
+import { UtilsService } from './../../../services/utils.service';
+import { User } from './../../../models/user.model';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.page.scss'],
 })
 export class SignUpPage implements OnInit {
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
 
-  constructor() { }
+  firebaseService = inject(FirebaseService);
+  utilsService = inject(UtilsService);
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async submit() {
+    if (this.form.valid) {
+      const loading = await this.utilsService.loading();
+      await loading.present();
+
+      this.firebaseService
+        .signUp(this.form.value as User)
+        .then(async (res) => {
+          await this.firebaseService.updateUser(this.form.value.name);
+          console.log(res);
+        })
+        .catch((errors) => {
+          this.utilsService.presentToast({
+            message: errors.message,
+            duration: 2500,
+            color: 'danger',
+            position: 'middle',
+            icon: 'alert-circle-outline',
+          });
+        })
+        .finally(() => loading.dismiss());
+    }
   }
-
 }
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,7 +1,12 @@
 import { Injectable, inject } from '@angular/core';
 
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from 'firebase/auth';
 import { User } from '../models/user.model';
 
 @Injectable({
@@ -12,5 +17,15 @@ export class FirebaseService {
 
   signIn(user: User) {
     return signInWithEmailAndPassword(getAuth(), user.email, user.password);
+  }
+
+  signUp(user: User) {
+    return createUserWithEmailAndPassword(getAuth(), user.email, user.password);
+  }
+
+  updateUser(displayName: string) {
+    return updateProfile(getAuth().currentUser, {
+      displayName,
+    });
   }
 }
